b2d1046407d8d21495d9676b849e0e7333bc08e4
NJI KEBEI DANIEL KOUM <60320246+Daniel-TheProgrammer@users.noreply.github.com>
2020-08-07 09:53:28 +0100

    Add files via upload

---
+++ celsius to fahrenheit.c
@@ -0,0 +1,19 @@
+//C program to convert Celsius to Fahrenheit by Daniel the Programmer
+#include <stdio.h>
+ 
+int main()
+{
+    float celsius, fahrenheit;
+ 
+    printf("Please Enter temperature in Celsius: \n");
+    scanf("%f", &celsius);
+ 
+    // Convert the temperature from celsius to fahrenheit 
+    fahrenheit = ((celsius * 9)/5) + 32;
+    // fahrenheit = ((9/5) * celsius) + 32;
+    // fahrenheit = ((1.8 * celsius) + 32;
+ 
+    printf("\n %.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
+ 
+    return 0;
+}
---
+++ celsius_table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int fahr,celsius;
+	int upper,lower,step;
+	lower=0;
+	upper=300;
+	step=20;
+	fahr=lower;
+	while(fahr<=upper){
+	celsius=5*(fahr-32)/9;
+	printf("%d\t%d\n",fahr,celsius);
+	fahr=fahr+step;
+	}
+
+}
